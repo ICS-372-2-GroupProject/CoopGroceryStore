ce5a3e23857fcf43ac6bdb9fb6149f8bb039f9a6
rkinsella <rkinsella@bes-32046>
2021-03-14 12:10:24 -0500

    Ryan testing Eclipse integration w/UI comment

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -36,15 +36,14 @@ import business.facade.Store;
 import business.tests.AutomatedTester;
 
 /**
- * TEST MAKING A CHANGE AND COMMITING This class implements the user interface
- * for the Store project. The commands are encoded as integers using a number of
- * static final variables. A number of utility methods exist to make it easier
- * to parse the input.
+ * RYAN TESTING. TEST MAKING A CHANGE AND COMMITING This class implements the
+ * user interface for the Store project. The commands are encoded as integers
+ * using a number of static final variables. A number of utility methods exist
+ * to make it easier to parse the input.
  */
 public class UserInterface {
 	private static UserInterface userInterface;
-	private BufferedReader reader = new BufferedReader(
-			new InputStreamReader(System.in));
+	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	private static Store store;
 	private static final int EXIT = 0;
 	private static final int ADD_MEMBER = 1;
@@ -58,16 +57,15 @@ public class UserInterface {
 	private static final int HELP = 9;
 
 	/**
-	 * Made private for singleton pattern. Conditionally looks for any saved
-	 * data. Otherwise, it gets a singleton Store object.
+	 * Made private for singleton pattern. Conditionally looks for any saved data.
+	 * Otherwise, it gets a singleton Store object.
 	 */
 	private UserInterface() {
 		if (yesOrNo("Look for saved data and  use it?")) {
 			retrieve();
 		} else {
 			store = store.instance();
-			if (yesOrNo(
-					"Do you want to generate a test bed and invoke the functionality using asserts? ")) {
+			if (yesOrNo("Do you want to generate a test bed and invoke the functionality using asserts? ")) {
 				new AutomatedTester().testAll();
 			}
 
@@ -77,6 +75,7 @@ public class UserInterface {
 
 	/**
 	 * Supports the singleton pattern
+	 * 
 	 * @return the singleton object
 	 */
 	public static UserInterface instance() {
@@ -89,6 +88,7 @@ public class UserInterface {
 
 	/**
 	 * Gets a token after prompting
+	 * 
 	 * @param prompt - whatever the user wants as prompt
 	 * @return - the token from the keyboard
 	 */
@@ -109,6 +109,7 @@ public class UserInterface {
 
 	/**
 	 * Gets a name after prompting
+	 * 
 	 * @param prompt - whatever the user wants as prompt
 	 * @return - the token from the keyboard
 	 */
@@ -127,6 +128,7 @@ public class UserInterface {
 
 	/**
 	 * Queries for a yes or no and returns true for yes and false for no
+	 * 
 	 * @param prompt The string to be prepended to the yes/no prompt
 	 * @return true for yes and false for no
 	 */
@@ -140,6 +142,7 @@ public class UserInterface {
 
 	/**
 	 * Converts the string to a number
+	 * 
 	 * @param prompt the string for prompting
 	 * @return the integer corresponding to the string
 	 */
@@ -157,6 +160,7 @@ public class UserInterface {
 
 	/**
 	 * Prompts for a date and gets a date object
+	 * 
 	 * @param prompt the prompt
 	 * @return the data as a Calendar object
 	 */
@@ -165,8 +169,7 @@ public class UserInterface {
 			try {
 				Calendar date = new GregorianCalendar();
 				String item = getToken(prompt);
-				DateFormat dateFormat = SimpleDateFormat
-						.getDateInstance(DateFormat.SHORT);
+				DateFormat dateFormat = SimpleDateFormat.getDateInstance(DateFormat.SHORT);
 				date.setTime(dateFormat.parse(item));
 				return date;
 			} catch (Exception fe) {
@@ -177,13 +180,13 @@ public class UserInterface {
 
 	/**
 	 * Prompts for a command from the keyboard
+	 * 
 	 * @return a valid command
 	 */
 	public int getCommand() {
 		do {
 			try {
-				int value = Integer.parseInt(
-						getToken("Enter command:" + HELP + " for help"));
+				int value = Integer.parseInt(getToken("Enter command:" + HELP + " for help"));
 				if (value >= EXIT && value <= HELP) {
 					return value;
 				}
@@ -197,8 +200,7 @@ public class UserInterface {
 	 * Displays the help screen
 	 */
 	public void help() {
-		System.out.println(
-				"Enter a number between 0 and ???? as explained below:");
+		System.out.println("Enter a number between 0 and ???? as explained below:");
 		System.out.println(EXIT + " to Exit\n");
 		System.out.println(ADD_MEMBER + " to add a member");
 		System.out.println(ADD_PRODUCT + " to  add product");
@@ -214,9 +216,8 @@ public class UserInterface {
 	}
 
 	/**
-	 * Method to be called for adding a member. Prompts the user for the
-	 * appropriate values and uses the appropriate Library method for adding the
-	 * member.
+	 * Method to be called for adding a member. Prompts the user for the appropriate
+	 * values and uses the appropriate Library method for adding the member.
 	 */
 	public void addMember() {
 		Request.instance().setMemberName(getName("Enter member name"));
@@ -226,21 +227,18 @@ public class UserInterface {
 		if (result.getResultCode() != Result.OPERATION_COMPLETED) {
 			System.out.println("Could not add member");
 		} else {
-			System.out.println(result.getMemberName() + "'s id is "
-					+ result.getMemberId());
+			System.out.println(result.getMemberName() + "'s id is " + result.getMemberId());
 		}
 	}
 
 	/**
-	 * Method to be called for adding a book. Prompts the user for the
-	 * appropriate values and uses the appropriate Library method for adding the
-	 * book.
+	 * Method to be called for adding a book. Prompts the user for the appropriate
+	 * values and uses the appropriate Library method for adding the book.
 	 */
 	public void addProduct() {
 		Request.instance().setProductName(getName("Enter  Product Name"));
 		Request.instance().setProductId(getToken("Enter Product id"));
-		Request.instance()
-				.setProductCurrentPrice(getName("Enter Product Current Price"));
+		Request.instance().setProductCurrentPrice(getName("Enter Product Current Price"));
 		Result result = store.addProduct(Request.instance());
 		if (result.getResultCode() != Result.OPERATION_COMPLETED) {
 			System.out.println("Book could not be added");
@@ -250,26 +248,24 @@ public class UserInterface {
 	}
 
 	/**
-	 * Method to be called for removing books. Prompts the user for the
-	 * appropriate values and uses the appropriate Library method for removing
-	 * books.
+	 * Method to be called for removing books. Prompts the user for the appropriate
+	 * values and uses the appropriate Library method for removing books.
 	 */
 	public void removeMember() {
 		Request.instance().setMemberId(getToken("Enter member id"));
 		Result result = store.removeMember(Request.instance());
 		switch (result.getResultCode()) {
-			case Result.NO_SUCH_MEMBER :
-				System.out.println("No such member with id "
-						+ Request.instance().getMemberId() + " in store");
-				break;
-			case Result.OPERATION_FAILED :
-				System.out.println("Member could not be removed");
-				break;
-			case Result.OPERATION_COMPLETED :
-				System.out.println(" Member has been removed");
-				break;
-			default :
-				System.out.println("An error has occurred");
+		case Result.NO_SUCH_MEMBER:
+			System.out.println("No such member with id " + Request.instance().getMemberId() + " in store");
+			break;
+		case Result.OPERATION_FAILED:
+			System.out.println("Member could not be removed");
+			break;
+		case Result.OPERATION_COMPLETED:
+			System.out.println(" Member has been removed");
+			break;
+		default:
+			System.out.println("An error has occurred");
 		}
 	}
 
@@ -280,14 +276,11 @@ public class UserInterface {
 	 */
 	public void getTransactions() {
 		Request.instance().setMemberId(getToken("Enter member id"));
-		Request.instance().setDate(getDate(
-				"Please enter the date for which you want records as mm/dd/yy"));
-		Iterator<Transaction> result = store
-				.getTransactions(Request.instance());
+		Request.instance().setDate(getDate("Please enter the date for which you want records as mm/dd/yy"));
+		Iterator<Transaction> result = store.getTransactions(Request.instance());
 		while (result.hasNext()) {
 			Transaction transaction = (Transaction) result.next();
-			System.out.println(transaction.getType() + "   "
-					+ transaction.getTitle() + "\n");
+			System.out.println(transaction.getType() + "   " + transaction.getTitle() + "\n");
 		}
 		System.out.println("\n End of transactions \n");
 	}
@@ -300,8 +293,7 @@ public class UserInterface {
 		System.out.println("List of members (name, address, phone, id)");
 		while (iterator.hasNext()) {
 			Result result = iterator.next();
-			System.out.println(result.getMemberName() + " "
-					+ result.getMemberAddress() + " " + result.getMemberPhone()
+			System.out.println(result.getMemberName() + " " + result.getMemberAddress() + " " + result.getMemberPhone()
 					+ " " + result.getMemberId());
 		}
 		System.out.println("End of listing");
@@ -312,14 +304,11 @@ public class UserInterface {
 	 */
 	public void getProduct() {
 		Iterator<Result> iterator = store.getProduct();
-		System.out.println(
-				"List of books (title, author, id, borrower id, due date)");
+		System.out.println("List of books (title, author, id, borrower id, due date)");
 		while (iterator.hasNext()) {
 			Result result = iterator.next();
-			System.out.println(
-					result.getProductName() + " " + result.getProductId() + " "
-							+ result.getProductCurrentPrice() + " "
-							+ result.getProductStockOnHand());
+			System.out.println(result.getProductName() + " " + result.getProductId() + " "
+					+ result.getProductCurrentPrice() + " " + result.getProductStockOnHand());
 		}
 		System.out.println("End of listing");
 	}
@@ -333,10 +322,8 @@ public class UserInterface {
 		if (result.getResultCode() != Result.OPERATION_COMPLETED) {
 			System.out.println("Could not process shipment");
 		} else {
-			System.out.println(result.getProductName() + "(Product ID: "
-					+ result.getProductId() + ") updated!");
-			System.out.println(
-					"New quantity in stock: " + result.getProductStockOnHand());
+			System.out.println(result.getProductName() + "(Product ID: " + result.getProductId() + ") updated!");
+			System.out.println("New quantity in stock: " + result.getProductStockOnHand());
 		}
 	}
 
@@ -346,8 +333,7 @@ public class UserInterface {
 	 */
 	private void save() {
 		if (store.save()) {
-			System.out.println(
-					" The library has been successfully saved in the file LibraryData \n");
+			System.out.println(" The library has been successfully saved in the file LibraryData \n");
 		} else {
 			System.out.println(" There has been an error in saving \n");
 		}
@@ -362,8 +348,7 @@ public class UserInterface {
 			if (store == null) {
 				store = Store.retrieve();
 				if (store != null) {
-					System.out.println(
-							" The Store has been successfully retrieved from the file StoreData \n");
+					System.out.println(" The Store has been successfully retrieved from the file StoreData \n");
 				} else {
 					System.out.println("File doesnt exist; creating new Store");
 					store = Store.instance();
@@ -383,39 +368,40 @@ public class UserInterface {
 		help();
 		while ((command = getCommand()) != EXIT) {
 			switch (command) {
-				case ADD_MEMBER :
-					addMember();
-					break;
-				case ADD_PRODUCT :
-					addProduct();
-					break;
-				case REMOVE_MEMBER :
-					removeMember();
-					break;
-				case GET_TRANSACTIONS :
-					getTransactions();
-					break;
-				case GET_MEMBERS :
-					getMembers();
-					break;
-				case GET_PRODUCT :
-					getProduct();
-					break;
-				case PROCESS_SHIPMENT :
-					processShipment();
-					break;
-				case SAVE :
-					save();
-					break;
-				case HELP :
-					help();
-					break;
+			case ADD_MEMBER:
+				addMember();
+				break;
+			case ADD_PRODUCT:
+				addProduct();
+				break;
+			case REMOVE_MEMBER:
+				removeMember();
+				break;
+			case GET_TRANSACTIONS:
+				getTransactions();
+				break;
+			case GET_MEMBERS:
+				getMembers();
+				break;
+			case GET_PRODUCT:
+				getProduct();
+				break;
+			case PROCESS_SHIPMENT:
+				processShipment();
+				break;
+			case SAVE:
+				save();
+				break;
+			case HELP:
+				help();
+				break;
 			}
 		}
 	}
 
 	/**
 	 * The method to start the application. Simply calls process().
+	 * 
 	 * @param args not used
 	 */
 	public static void main(String[] args) {
