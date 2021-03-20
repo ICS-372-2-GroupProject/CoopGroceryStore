34317e53c97430f88044e217c33ca2a9f4cb8b5f
marcw <marcw@LAPTOP-LHBH8DG6>
2021-03-20 15:11:40 -0500

    Added a loop to the processShipment method (and some other changes).

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -43,8 +43,7 @@ import business.tests.AutomatedTester;
  */
 public class UserInterface {
 	private static UserInterface userInterface;
-	private BufferedReader reader = new BufferedReader(
-			new InputStreamReader(System.in));
+	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	private static Store store;
 	private static final int EXIT = 0;
 	private static final int ENROLL_MEMBER = 1;
@@ -63,16 +62,15 @@ public class UserInterface {
 	private static final int HELP = 14;
 
 	/**
-	 * Made private for singleton pattern. Conditionally looks for any saved
-	 * data. Otherwise, it gets a singleton Store object.
+	 * Made private for singleton pattern. Conditionally looks for any saved data.
+	 * Otherwise, it gets a singleton Store object.
 	 */
 	private UserInterface() {
 		if (yesOrNo("Look for saved data and  use it?")) {
 			retrieve();
 		} else {
 			store = store.instance();
-			if (yesOrNo(
-					"Do you want to generate a test bed and invoke the functionality using asserts? ")) {
+			if (yesOrNo("Do you want to generate a test bed and invoke the functionality using asserts? ")) {
 				new AutomatedTester().testAll();
 			}
 
@@ -82,6 +80,7 @@ public class UserInterface {
 
 	/**
 	 * Supports the singleton pattern
+	 * 
 	 * @return the singleton object
 	 */
 	public static UserInterface instance() {
@@ -94,7 +93,9 @@ public class UserInterface {
 
 	/**
 	 * Gets a token after prompting
-	 * @param prompt - whatever the user wants as prompt
+	 * 
+	 * @param prompt
+	 *            - whatever the user wants as prompt
 	 * @return - the token from the keyboard
 	 */
 	public String getToken(String prompt) {
@@ -114,7 +115,9 @@ public class UserInterface {
 
 	/**
 	 * Gets a name after prompting
-	 * @param prompt - whatever the user wants as prompt
+	 * 
+	 * @param prompt
+	 *            - whatever the user wants as prompt
 	 * @return - the token from the keyboard
 	 */
 	public String getName(String prompt) {
@@ -132,7 +135,9 @@ public class UserInterface {
 
 	/**
 	 * Queries for a yes or no and returns true for yes and false for no
-	 * @param prompt The string to be prepended to the yes/no prompt
+	 * 
+	 * @param prompt
+	 *            The string to be prepended to the yes/no prompt
 	 * @return true for yes and false for no
 	 */
 	private boolean yesOrNo(String prompt) {
@@ -145,7 +150,9 @@ public class UserInterface {
 
 	/**
 	 * Converts the string to a number
-	 * @param prompt the string for prompting
+	 * 
+	 * @param prompt
+	 *            the string for prompting
 	 * @return the integer corresponding to the string
 	 */
 	public int getNumber(String prompt) {
@@ -162,7 +169,9 @@ public class UserInterface {
 
 	/**
 	 * Prompts for a date and gets a date object
-	 * @param prompt the prompt
+	 * 
+	 * @param prompt
+	 *            the prompt
 	 * @return the data as a Calendar object
 	 */
 	public Calendar getDate(String prompt) {
@@ -170,8 +179,7 @@ public class UserInterface {
 			try {
 				Calendar date = new GregorianCalendar();
 				String item = getToken(prompt);
-				DateFormat dateFormat = SimpleDateFormat
-						.getDateInstance(DateFormat.SHORT);
+				DateFormat dateFormat = SimpleDateFormat.getDateInstance(DateFormat.SHORT);
 				date.setTime(dateFormat.parse(item));
 				return date;
 			} catch (Exception fe) {
@@ -182,13 +190,13 @@ public class UserInterface {
 
 	/**
 	 * Prompts for a command from the keyboard
+	 * 
 	 * @return a valid command
 	 */
 	public int getCommand() {
 		do {
 			try {
-				int value = Integer.parseInt(
-						getToken("Enter command:" + HELP + " for help"));
+				int value = Integer.parseInt(getToken("Enter command:" + HELP + " for help"));
 				if (value >= EXIT && value <= HELP) {
 					return value;
 				}
@@ -202,8 +210,7 @@ public class UserInterface {
 	 * Displays the help screen
 	 */
 	public void help() {
-		System.out.println(
-				"Enter a number between 0 and ???? as explained below:");
+		System.out.println("Enter a number between 0 and ???? as explained below:");
 		System.out.println("\n" + EXIT + " to Exit");
 		System.out.println(ENROLL_MEMBER + " to enroll a member");
 		System.out.println(REMOVE_MEMBER + " to remove a member");
@@ -234,21 +241,19 @@ public class UserInterface {
 		if (result.getResultCode() != Result.OPERATION_COMPLETED) {
 			System.out.println("Could not add member");
 		} else {
-			System.out.println(result.getMemberName() + "'s id is "
-					+ result.getMemberId());
+			System.out.println(result.getMemberName() + "'s id is " + result.getMemberId());
 		}
 	}
 
 	/**
 	 * code updated by Richard Fritz Method to be called for adding a product.
-	 * Prompts the user for the appropriate values and uses the appropriate
-	 * Store method for adding the product.
+	 * Prompts the user for the appropriate values and uses the appropriate Store
+	 * method for adding the product.
 	 */
 	public void addProduct() {
 		Request.instance().setProductName(getName("Enter  Product Name"));
 		Request.instance().setProductId(getToken("Enter Product id"));
-		Request.instance()
-				.setProductCurrentPrice(getName("Enter Product Current Price"));
+		Request.instance().setProductCurrentPrice(getName("Enter Product Current Price"));
 		Result result = store.addProduct(Request.instance());
 		if (result.getResultCode() != Result.OPERATION_COMPLETED) {
 			System.out.println("Product could not be added");
@@ -266,18 +271,17 @@ public class UserInterface {
 		Request.instance().setMemberId(getToken("Enter member id"));
 		Result result = store.removeMember(Request.instance());
 		switch (result.getResultCode()) {
-			case Result.NO_SUCH_MEMBER :
-				System.out.println("No such member with id "
-						+ Request.instance().getMemberId() + " in member list");
-				break;
-			case Result.OPERATION_FAILED :
-				System.out.println("Member could not be removed");
-				break;
-			case Result.OPERATION_COMPLETED :
-				System.out.println(" Member has been removed");
-				break;
-			default :
-				System.out.println("An error has occurred");
+		case Result.NO_SUCH_MEMBER:
+			System.out.println("No such member with id " + Request.instance().getMemberId() + " in member list");
+			break;
+		case Result.OPERATION_FAILED:
+			System.out.println("Member could not be removed");
+			break;
+		case Result.OPERATION_COMPLETED:
+			System.out.println(" Member has been removed");
+			break;
+		default:
+			System.out.println("An error has occurred");
 		}
 	}
 
@@ -288,14 +292,11 @@ public class UserInterface {
 	 */
 	public void getTransactions() {
 		Request.instance().setMemberId(getToken("Enter member id"));
-		Request.instance().setDate(getDate(
-				"Please enter the date for which you want records as mm/dd/yy"));
-		Iterator<Transaction> result = store
-				.getTransactions(Request.instance());
+		Request.instance().setDate(getDate("Please enter the date for which you want records as mm/dd/yy"));
+		Iterator<Transaction> result = store.getTransactions(Request.instance());
 		while (result.hasNext()) {
 			Transaction transaction = (Transaction) result.next();
-			System.out.println(transaction.getType() + "   "
-					+ transaction.getTitle() + "\n");
+			System.out.println(transaction.getType() + "   " + transaction.getTitle() + "\n");
 		}
 		System.out.println("\n End of transactions \n");
 	}
@@ -305,14 +306,11 @@ public class UserInterface {
 	 */
 	public void getMembers() {
 		Iterator<Result> iterator = store.getMembers();
-		System.out.println(
-				"List of members (Member Name, address, phone, date joined, fee paid, Member Id)");
+		System.out.println("List of members (Member Name, address, phone, date joined, fee paid, Member Id)");
 		while (iterator.hasNext()) {
 			Result result = iterator.next();
-			System.out.println(result.getMemberName() + " "
-					+ result.getMemberAddress() + " " + result.getMemberPhone()
-					+ " " + result.getMemberDateJoined() + " "
-					+ result.getMemberFee() + " " + result.getMemberId());
+			System.out.println(result.getMemberName() + " " + result.getMemberAddress() + " " + result.getMemberPhone()
+					+ " " + result.getMemberDateJoined() + " " + result.getMemberFee() + " " + result.getMemberId());
 		}
 		System.out.println("End of listing");
 	}
@@ -322,14 +320,11 @@ public class UserInterface {
 	 */
 	public void getProduct() {
 		Iterator<Result> iterator = store.getProduct();
-		System.out.println(
-				"List of product (Product Name, Stock on Hand, Current Price, Reorder Level, Product Id)");
+		System.out.println("List of product (Product Name, Stock on Hand, Current Price, Reorder Level, Product Id)");
 		while (iterator.hasNext()) {
 			Result result = iterator.next();
-			System.out.println(result.getProductName() + " "
-					+ result.getProductStockOnHand() + " "
-					+ result.getProductCurrentPrice() + " "
-					+ result.getProductReorderLevel() + " "
+			System.out.println(result.getProductName() + " " + result.getProductStockOnHand() + " "
+					+ result.getProductCurrentPrice() + " " + result.getProductReorderLevel() + " "
 					+ result.getProductId());
 		}
 		System.out.println("End of listing");
@@ -339,16 +334,19 @@ public class UserInterface {
 	 * Method to be called for processing a shipment.
 	 */
 	public void processShipment() {
-		Request.instance().setOrderId(getName("Enter Order ID"));
-		Result result = store.processShipment(Request.instance());
-		if (result.getResultCode() != Result.OPERATION_COMPLETED) {
-			System.out.println("Could not process shipment");
-		} else {
-			System.out.println(result.getProductName() + "(Product ID: "
-					+ result.getProductId() + ") updated!");
-			System.out.println(
-					"New quantity in stock: " + result.getProductStockOnHand());
-		}
+		do {
+			Request.instance().setOrderId(getToken("Enter Order ID"));
+			Result result = store.processShipment(Request.instance());
+			if (result.getResultCode() == Result.OPERATION_COMPLETED) {
+				System.out.println(result.getProductName() + "(Product ID: " + result.getProductId() + ") updated!");
+				System.out.println("New quantity in stock: " + result.getProductStockOnHand());
+			} else {
+				System.out.println("Could not process shipment");
+			}
+			if (!yesOrNo("Process more orders?")) {
+				break;
+			}
+		} while (true);
 	}
 
 	/**
@@ -357,8 +355,7 @@ public class UserInterface {
 	 */
 	private void save() {
 		if (store.save()) {
-			System.out.println(
-					" The library has been successfully saved in the file LibraryData \n");
+			System.out.println(" The library has been successfully saved in the file LibraryData \n");
 		} else {
 			System.out.println(" There has been an error in saving \n");
 		}
@@ -373,8 +370,7 @@ public class UserInterface {
 			if (store == null) {
 				store = Store.retrieve();
 				if (store != null) {
-					System.out.println(
-							" The Store has been successfully retrieved from the file StoreData \n");
+					System.out.println(" The Store has been successfully retrieved from the file StoreData \n");
 				} else {
 					System.out.println("File doesnt exist; creating new Store");
 					store = Store.instance();
@@ -394,49 +390,51 @@ public class UserInterface {
 		help();
 		while ((command = getCommand()) != EXIT) {
 			switch (command) {
-				case ENROLL_MEMBER :
-					enrollMember();
-					break;
-				case REMOVE_MEMBER :
-					removeMember();
-					break;
-				case ADD_PRODUCT :
-					addProduct();
-					break;
-				case name4 :
-					break;
-				case PROCESS_SHIPMENT :
-					processShipment();
-					break;
-				case name6 :
-					break;
-				case name7 :
-					break;
-				case name8 :
-					break;
-				case name9 :
-					break;
-				case name10 :
-					break;
-				case GET_MEMBERS :
-					getMembers();
-					break;
-				case GET_PRODUCT :
-					getProduct();
-					break;
-				case SAVE :
-					save();
-					break;
-				case HELP :
-					help();
-					break;
+			case ENROLL_MEMBER:
+				enrollMember();
+				break;
+			case REMOVE_MEMBER:
+				removeMember();
+				break;
+			case ADD_PRODUCT:
+				addProduct();
+				break;
+			case name4:
+				break;
+			case PROCESS_SHIPMENT:
+				processShipment();
+				break;
+			case name6:
+				break;
+			case name7:
+				break;
+			case name8:
+				break;
+			case name9:
+				break;
+			case name10:
+				break;
+			case GET_MEMBERS:
+				getMembers();
+				break;
+			case GET_PRODUCT:
+				getProduct();
+				break;
+			case SAVE:
+				save();
+				break;
+			case HELP:
+				help();
+				break;
 			}
 		}
 	}
 
 	/**
 	 * The method to start the application. Simply calls process().
-	 * @param args not used
+	 * 
+	 * @param args
+	 *            not used
 	 */
 	public static void main(String[] args) {
 		UserInterface.instance().process();
